c92fdf85dc07b36a1856c180f2cf6dc203646114
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-11-14 21:01:37 -0300

    Inicio com erros

--- src/routes/pilha.ts
+++ src/routes/pilha.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express";
-import { push, pop } from ".";
+//import { push, pop } from ".";
 
 const routes = Router();
 
 
-routes.get("/push/:nome", push);
-routes.get("/pop", pop);
+routes.get("/push/:nome", );//push);
+routes.get("/pop", )///pop);
 
 //aceita qualquer método HTTP ou URL
 routes.use( (_:Request,res:Response) => res.json({error:"Requisição desconhecida"}) );
